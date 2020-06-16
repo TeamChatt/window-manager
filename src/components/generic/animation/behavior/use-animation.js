cd7e0d0cb625cc4072c74c1f037d7ac86a46549a
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2020-06-16 16:06:10 +0900

    Return from useLayoutEffect so action can be canceled

--- src/components/generic/animation/behavior/use-animation.js
+++ src/components/generic/animation/behavior/use-animation.js
@@ -4,9 +4,7 @@ const useAnimation = (ref, inProp, animateIn, animateOut) => {
   const runEnter = () => animateIn(ref.current)
   const runExit = () => animateOut(ref.current)
 
-  useLayoutEffect(() => {
-    (inProp ? runEnter() : runExit())
-  }, [inProp])
+  useLayoutEffect(() => (inProp ? runEnter() : runExit()), [inProp])
 }
 
 export default useAnimation
